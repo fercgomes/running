733822d03b5134efec8d23edb99345fb1ee0d79a
Fernando Gomes <fcgomes@inf.ufrgs.br>
2022-09-13 23:21:33 -0300

    feat: fixing try catch

--- src/lib/sheets/index.ts
+++ src/lib/sheets/index.ts
@@ -5,33 +5,38 @@ const doc = new GoogleSpreadsheet(
 );
 
 export async function getSportsEventsSchedule() {
-  try {
-    const serviceAccountEmail = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL || "";
-    const serviceAccountPrivateKey = process.env.GOOGLE_PRIVATE_KEY || "";
+  const serviceAccountEmail = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
+  const serviceAccountPrivateKey = process.env.GOOGLE_PRIVATE_KEY;
 
-    await doc.useServiceAccountAuth({
-      client_email: serviceAccountEmail,
-      private_key: serviceAccountPrivateKey,
-    });
-
-    await doc.loadInfo();
-
-    const sheet = doc.sheetsByIndex[0];
-    const rows = await sheet.getRows();
-
-    return rows.map((row) => {
-      return {
-        name: row.name,
-        type: row.type,
-        confirmed_date: row.confirmed_date,
-        surface_type: row.surface_type,
-        city_name: row.city_name,
-        state: row.state,
-        country: row.country,
-        distances: row.distances.split(","),
-      };
-    });
-  } catch (error) {
-    return null;
+  if (
+    serviceAccountEmail === undefined ||
+    serviceAccountEmail.length === 0 ||
+    serviceAccountPrivateKey === undefined ||
+    serviceAccountPrivateKey.length === 0
+  ) {
+    throw new Error("No Sheets Api Key");
   }
+
+  await doc.useServiceAccountAuth({
+    client_email: serviceAccountEmail,
+    private_key: serviceAccountPrivateKey,
+  });
+
+  await doc.loadInfo();
+
+  const sheet = doc.sheetsByIndex[0];
+  const rows = await sheet.getRows();
+
+  return rows.map((row) => {
+    return {
+      name: row.name,
+      type: row.type,
+      confirmed_date: row.confirmed_date,
+      surface_type: row.surface_type,
+      city_name: row.city_name,
+      state: row.state,
+      country: row.country,
+      distances: row.distances.split(","),
+    };
+  });
 }
